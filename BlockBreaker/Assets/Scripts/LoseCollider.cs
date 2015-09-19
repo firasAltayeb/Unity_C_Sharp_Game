095e90e1bc38d977e0546fcd37e55697fe69b158
FIRAS ALTAYEB <tayebfiras@gmail.com>
2015-09-19 15:52:36 +0100

    Added a paddel to level_0 and used lose collider Added a brick sprite as player paddle, gave the paddle a rigid body and a collider. Made the lose collider load the next level.

--- BlockBreaker/Assets/Scripts/LoseCollider.cs
+++ BlockBreaker/Assets/Scripts/LoseCollider.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 
 public class LoseCollider : MonoBehaviour {
-
+		
+		public LevelManager levelManager;
 		void OnTriggerEnter2D (Collider2D trigger) {
 			print ("Trigger");
+			levelManager.LoadLevel("winScreen");
 		}
 		
 		void OnCollisionEnter2D (Collision2D collision) {
