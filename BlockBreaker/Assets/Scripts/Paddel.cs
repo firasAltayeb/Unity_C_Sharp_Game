65e759b9ca4ac25a2916b6d90e05799b20218690
FIRAS ALTAYEB <tayebfiras@gmail.com>
2015-09-22 23:39:01 +0100

    Added a script to the paddel Used "Input.MousePostion.x" and "Screen.Width" to make the paddel move with the mouse, also used "Mathf.Clamp" to constrain the paddel to the game screen. Fixed the paddel pixels per unit to 128 so that it could match the game world unit.

---
+++ BlockBreaker/Assets/Scripts/Paddel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddel : MonoBehaviour {
+	
+	void Update () {
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+		Vector3 paddlePos = new Vector3(0.5f,this.transform.position.y,-0.5f);
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+		this.transform.position = paddlePos;
+		
+	}
+}
