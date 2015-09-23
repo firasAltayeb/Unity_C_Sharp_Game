864540565754c7fab68736190f838d8f1f085dec
FIRAS ALTAYEB <tayebfiras@gmail.com>
2015-09-23 23:34:36 +0100

    Added a movement controlling script to the ball Captured the relative position from the editor, launched the ball by "Input.GetMouseButtonDown(0)", created and used the variable hasStarted to keep track of weather the game started or not.

---
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Paddel paddel;
+	private bool hasStarted = false;
+	private Vector3 paddelToBallVector;
+	
+	void Start () {
+			paddelToBallVector = this.transform.position - paddel.transform.position;	
+	}
+	
+	void Update () {
+		if(!hasStarted) {
+			//lock the ball relative to the paddle.
+			this.transform.position = paddel.transform.position + paddelToBallVector;
+			//wait for a mouse press to launch.
+			if(Input.GetMouseButtonDown(0)) {
+				hasStarted = true;
+				this.rigidbody2D.velocity = new Vector2(2f, 10f);
+			}
+		}
+	}
+	
+
+}
