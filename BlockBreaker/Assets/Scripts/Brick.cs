5f501789b7f5f211acc7cdd74a0f60b3a900dccc
FIRAS ALTAYEB <tayebfiras@gmail.com>
2015-09-25 23:35:33 +0100

    set up prefabs Made three different prefabs and assgined them to a script which counts the number of times they have been hit, Adjusted the prefabs to suit the game needs.

---
+++ BlockBreaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+	public int maxHits;
+	private int timesHits;
+	
+	void Start () {
+		timesHits = 0;
+	}
+	
+	void Update () {
+	}
+	
+	void OnCollisionEnter2D (Collision2D col) {
+		timesHits++;
+	}
+	
+}
