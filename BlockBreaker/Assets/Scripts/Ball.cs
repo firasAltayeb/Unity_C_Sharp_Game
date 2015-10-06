f101f3ee49c83a72d562783edb7643f96e8fbbfe
FIRAS ALTAYEB <tayebfiras@gmail.com>
2015-10-06 23:38:56 +0100

    Stop boring loop and custiomized level_3 Stop the boring loop in the scenes that occur when the loop keep on hitting the scene's walls instead of the bricks by using the Random.Range(), Added multiple bricks to level_3.

--- BlockBreaker/Assets/Scripts/Ball.cs
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -25,6 +25,8 @@ public class Ball : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter2D (Collision2D collision) {
+		Vector2 tweak = new Vector2(Random.Range(1f, 1f),Random.Range(3f, 1.5f));
+		rigidbody2D.velocity += tweak;
 		if  (hasStarted) {
 			audio.Play ();
 		}
