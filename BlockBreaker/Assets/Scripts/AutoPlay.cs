7994efd2824fb9d0f2298b25c1d1348d36fc2636
FIRAS ALTAYEB <tayebfiras@gmail.com>
2015-10-12 03:04:21 +0100

    Fixed an error with the autoPlay object Added a script in the loseScreen which destroys the autoPlay object and prevents it from going to the startScreen, by doing so the player is now capable of changing the game from auto play to self play at any point. Also changed the paddle and ball Y axis in all the scenes in order for them to be visable in an andriod build.

--- BlockBreaker/Assets/Scripts/AutoPlay.cs
+++ BlockBreaker/Assets/Scripts/AutoPlay.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class AutoPlay : MonoBehaviour {
 	
-	static AutoPlay instance  = null;
+	static AutoPlay instance = null;
 	public bool autoPlay = false;
-	
+
 	public void autoMode() {
 		autoPlay = true;
-		// print (autoPlay == true);
+		print (autoPlay == true);
 	}
 	
 	void Awake () {
---
+++ BlockBreaker/Assets/Scripts/ReStarter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReStarter : MonoBehaviour {
+	
+	private AutoPlay au;
+	
+	void Start () {
+		au = GameObject.FindObjectOfType<AutoPlay>();
+		Destroy (au);
+	}
+	
+}
